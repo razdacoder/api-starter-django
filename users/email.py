92aeabf828c0676024d55761f85cebed9262a338
ramonrash2 <ramonrash2@gmail.com>
2024-12-28 16:11:05 +0000

    added user functions

---
+++ users/email.py
@@ -0,0 +1,76 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+
+from . import utils
+from django.conf import settings as django_settings
+from .config import settings
+from core.email import BaseEmailMessage
+
+
+
+class BaseUserEmail(BaseEmailMessage):
+    def get_context_data(self):
+        context = super().get_context_data()
+        overridable = {
+            "protocol": settings.EMAIL_FRONTEND_PROTOCOL,
+            "domain": settings.EMAIL_FRONTEND_DOMAIN,
+            "site_name": settings.EMAIL_FRONTEND_SITE_NAME,
+        }
+        for context_key, context_value in overridable.items():
+            if context_value:
+                context.update({context_key: context_value})
+        context.pop("view", None)
+        return context
+
+
+class ActivationEmail(BaseUserEmail):
+    template_name = "email/activation.html"
+
+    def get_context_data(self):
+        # ActivationEmail can be deleted
+        context = super().get_context_data()
+
+        user = context.get("user")
+        context["uid"] = utils.encode_uid(user.pk)
+        context["token"] = default_token_generator.make_token(user)
+        context["url"] = settings.ACTIVATION_URL.format(**context)
+        return context
+
+
+class ConfirmationEmail(BaseUserEmail):
+    template_name = "email/confirmation.html"
+
+
+class PasswordResetEmail(BaseUserEmail):
+    template_name = "email/password_reset.html"
+
+    def get_context_data(self):
+        # PasswordResetEmail can be deleted
+        context = super().get_context_data()
+
+        user = context.get("user")
+        context["uid"] = utils.encode_uid(user.pk)
+        context["token"] = default_token_generator.make_token(user)
+        context["url"] = settings.PASSWORD_RESET_CONFIRM_URL.format(**context)
+        return context
+
+
+class PasswordChangedConfirmationEmail(BaseUserEmail):
+    template_name = "email/password_changed_confirmation.html"
+
+
+# class UsernameChangedConfirmationEmail(BaseDjoserEmail):
+#     template_name = "email/username_changed_confirmation.html"
+
+
+# class UsernameResetEmail(BaseDjoserEmail):
+    template_name = "email/username_reset.html"
+
+    def get_context_data(self):
+        context = super().get_context_data()
+
+        user = context.get("user")
+        context["uid"] = utils.encode_uid(user.pk)
+        context["token"] = default_token_generator.make_token(user)
+        context["url"] = settings.USERNAME_RESET_CONFIRM_URL.format(**context)
+        return context
